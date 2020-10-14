4e8c850d2508e14d98ef0bd80518d5ae9b463e35
Chris Bram <cpbram2011@gmail.com>
2020-10-13 19:15:13 -0500

    Add styling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import "./styles/index.scss";
 import "./styles/dropdown.scss";
+// import * as WaveformSelector from './scripts/waveform'
+// console.log(WaveformSelector.synthParams)
 
+
+
+const square = new Tone.Synth({
+  oscillator: {
+    type: 'square'
+  }
+}).toDestination()
+const filter = new Tone.Filter().toDestination()
+filter.type = 'lowpass';
 const osc = new Tone.PolySynth().toDestination()
 const bendosc = new Tone.Synth().toDestination()
 const vib = new Tone.Vibrato(5, 0.1).toDestination()
 const vibosc = new Tone.Synth().toDestination()
 vibosc.connect(vib);
+osc.connect(filter)
 const now = Tone.now();
-
 let down = {};
 let voices = {};
-window.voices = voices;
+
+
+
 // const pitches = ['E2','F2','Gb2','G2','Ab2','A2','Bb2','B2',   //octave down
 // 'C3','Db3','D3','Eb3','E3','F3','Gb3','G3','Ab3','A3','Bb3','B3',
 // 'C4','Db4','D4','Eb4','E4','F4','Gb4']
@@ -25,8 +38,7 @@ const revPitches = pitches.map((x,i) => pitches[pitches.length - i - 1])
 // 'C5','D5','E5','F5','Gb5','G5','Ab5','A5','B5',]
 
 // const pitches = ['C2','D2','E2','G2','A2','C3','D3','E3','G3','A3','C4','D4','E4','G4','A4','C5','D5','E5','G5','A5'] // pentatonic
-console.log(pitches)
-console.log(revPitches)
+
 const keys = "zxcvbnm,./asdfghjkl;'qwertyuiop[]1234567890-=".split('')
 
 keys.forEach(p => {
@@ -57,10 +69,16 @@ let bend = {
   start: null,
   end: null
 };
-
 document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("keydown", e => {
-    
+    // console.log(e.key)
+    // console.log(keyMap[e.key])
+    if (e.keyCode === 32){
+      e.preventDefault()
+    }
+
+
+
     if ( down[e.key] === 0) {
       down[e.key] = 1;
       voices[keyMap[e.key]] = 1;
@@ -94,12 +112,20 @@ document.addEventListener("DOMContentLoaded", () => {
       if (pressed) pressed.classList.add('pressed')
     }
   });
+
+    //   var autoWah = new Tone.AutoWah(60, 4, -30).toMaster();
+    // //initialize the synth and connect to autowah
+    // var synth = osc.connect(autoWah);
+    // //Q value influences the effect of the wah - default is 2
+    // autoWah.Q.value = 6;
+    // //more audible on higher notes
   
   
   
   
   document.addEventListener("keyup", (e) => {
     if (e.location === KeyboardEvent.DOM_KEY_LOCATION_RIGHT){
+      debugger
       bendosc.frequency.rampTo(bend.start, 0.2);
     }
     if (e.key === 'Enter') {
@@ -119,5 +145,24 @@ document.addEventListener("DOMContentLoaded", () => {
     if (unpressed) unpressed.classList.remove('pressed')
     }, false)
 
-
+    document.addEventListener("click", e => {
+      if (['sine','triangle','square','sawtooth'].includes(e.target.classList[0])){
+      const old = document.getElementsByClassName('selected')[0]
+      if (old) old.classList.remove('selected')
+      e.target.classList.add('selected')
+      // synthParams.type = e.target.classList[0]
+      
+      osc.set({
+        'oscillator': {
+          'type': e.target.classList[0]
+        }
+      });
+      bendosc.set({
+        'oscillator': {
+          'type': e.target.classList[0]
+        }
+      });
+      
+    }
+  })
 });
